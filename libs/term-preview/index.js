a77c91c6d74642b7a54c18bdbbc516680c966a09
Iliana Betsou <ilianampe@admin.grnet.gr>
2021-08-05 12:59:27 +0000

    feat(term-preview): add new term tooltip

--- libs/term-preview/index.js
+++ libs/term-preview/index.js
@@ -1,42 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import Postel from 'postel';
-import BrowserOnly from '@docusaurus/BrowserOnly';
-import RcTooltip from 'rc-tooltip';
-import 'rc-tooltip/assets/bootstrap_white.css';
-
-const style = {
-  boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
-  transition: '0.3s',
-  padding: '8px',
-  background: 'white',
-  fontSize: '13px'
-}
+import React, { useEffect, useState } from "react";
+import Postel from "postel";
+import BrowserOnly from "@docusaurus/BrowserOnly";
+import RcTooltip from "rc-tooltip";
+import "rc-tooltip/assets/bootstrap_white.css";
+import "./tooltip-styles.css";
 
 const caret = {
-  height: '10px',
-  width: '10px',
-  background: 'rgb(255, 255, 255)',
-  clipPath: 'polygon(100% 0px, 0% 100%, 100% 100%)',
-  filter: 'drop-shadow(rgba(50, 50, 0, 0.5) -1px 16px 6px)'
-}
+  height: "10px",
+  width: "10px",
+  background: "rgb(255, 255, 255)",
+  clipPath: "polygon(100% 0px, 0% 100%, 100% 100%)",
+  filter: "drop-shadow(rgba(50, 50, 0, 0.5) -1px 16px 6px)",
+};
 
 const link = {
-  textDecoration: 'underline'
-}
+  textDecoration: "underline",
+};
 
-const Content = React.forwardRef(({ setContent, content, url }, ref) => {
+const Content = React.forwardRef(({ setContent, content, url, theme }, ref) => {
   if (typeof window !== undefined) {
     if (!window._cachedTerms) {
       window._cachedTerms = {};
     }
   }
+
   useEffect(() => {
     const JSONurl = `${url}.json`;
     if (!content) {
       if (!window._cachedTerms[JSONurl]) {
         fetch(JSONurl)
-          .then(res => res.json())
-          .then(obj => {
+          .then((res) => res.json())
+          .then((obj) => {
             setContent(obj);
             window._cachedTerms[JSONurl] = obj;
           });
@@ -44,34 +38,47 @@ const Content = React.forwardRef(({ setContent, content, url }, ref) => {
         setContent(window._cachedTerms[JSONurl]);
       }
     }
-  }, [content, url])
-  return <div style={style} ref={ref}>{content ?
-    <>
-      <h4>{content.metadata.title}</h4>
-      <p>{content.metadata.hoverText}</p>
-    </> :
-    'loading...'}
-  </div>
+  }, [content, url]);
+  return (
+    <div className={`mytooltip ${theme}`} ref={ref}>
+      {content ? (
+        <>
+          <h4>{content.metadata.title}</h4>
+          <p>{content.metadata.hoverText}</p>
+        </>
+      ) : (
+        "loading..."
+      )}
+    </div>
+  );
 });
 
 const Tooltip = (props) => {
   const { children, pathName } = props;
   const [content, setContent] = useState();
-  const cleanPathname = pathName.replace(/\/$/,'');
+  const cleanPathname = pathName.replace(/\/$/, "");
 
   return (
     <BrowserOnly
-      fallback={<div>The fallback content to display on prerendering</div>}>
-      {() =>
+      fallback={<div>The fallback content to display on prerendering</div>}
+    >
+      {() => (
         <RcTooltip
           placement="top"
-          overlay={<Content className="rc-tooltip-arrow-inner" url={cleanPathname} content={content} setContent={setContent} />}
+          overlay={
+            <Content
+              url={cleanPathname}
+              content={content}
+              setContent={setContent}
+              theme="white"
+            />
+          }
         >
           <a style={link} href={pathName}>
             {children}
           </a>
         </RcTooltip>
-      }
+      )}
     </BrowserOnly>
   );
 };
