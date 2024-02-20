f2cde50e6a6fca2aaae25a82d022e791825e5830
Vasia Koumarela <vasiakoum@admin.grnet.gr>
2024-02-21 00:33:41 +0200

    fix(plugins): support multiline link for only one newline

--- plugins/webpack-terms-replace-loader/index.js
+++ plugins/webpack-terms-replace-loader/index.js
@@ -7,7 +7,7 @@ const root = process.platform === 'win32' ? path.win32.dirname(pkg) : path.dirna
 
 module.exports = function(source) {
   const urlsRegex = /(?<!!)\[[^\]]+\]\([^)]+\)/g;
-  const urlRegex = /\[(.*?)\]\((.*?)\)/;
+  const urlRegex =  /\[\s*(.*?)\s*\]\((.*?)\)/s;
   const urls = source.match(urlsRegex) || [];
   const importStatement = `
 import Term from "${ this.query.termPreviewComponentPath || "@grnet/docusaurus-term-preview"}";
@@ -17,17 +17,21 @@ import Term from "${ this.query.termPreviewComponentPath || "@grnet/docusaurus-t
     source = source.replace(content, importStatement + content);
     for (const url of urls) {
       const [mdUrl, title, urlPath] = url.match(urlRegex);
-      const rel_path = process.platform === 'win32' ? path.win32.relative(root, this.resourcePath) : path.relative(root, this.resourcePath);
-      const pathName = new URL(urlPath, `http://bla.com/${rel_path}`).pathname;
-      if (pathName.includes(this.query.termsDir.replace(/\./, ''))) {
-        const termKey =
-          this.query.baseUrl.replace(/\/$/, '') +
-          pathName.replace(/\.(md|mdx)$/, '');
-        const metadata = store.terms[termKey];
-        source = source.replace(
-          mdUrl,
-          `<Term pathName="${termKey.replace(/\d+-/, '')}">${title}</Term>`
-        );
+      const newLineRegex = /\n/g;
+      const newLineCount = (title.match(newLineRegex) || []).length;
+      if (newLineCount <= 1) {
+        const rel_path = process.platform === 'win32' ? path.win32.relative(root, this.resourcePath) : path.relative(root, this.resourcePath);
+        const pathName = new URL(urlPath, `http://bla.com/${rel_path}`).pathname;
+        if (pathName.includes(this.query.termsDir.replace(/\./, ''))) {
+          const termKey =
+            this.query.baseUrl.replace(/\/$/, '') +
+            pathName.replace(/\.(md|mdx)$/, '');
+          const metadata = store.terms[termKey];
+          source = source.replace(
+            mdUrl,
+            `<Term pathName="${termKey.replace(/\d+-/, '')}">${title}</Term>`
+          );
+        }
       }
     }
   }
