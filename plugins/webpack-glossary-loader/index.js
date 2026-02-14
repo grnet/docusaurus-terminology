61b50d4a53ea147d49fc11501eacf5968346ef28
Bart Kamphorst <bart.kamphorst@tno.nl>
2026-02-14 18:25:18 +0100

    fix: correctly add import to glossary with frontmatter

--- plugins/webpack-glossary-loader/index.js
+++ plugins/webpack-glossary-loader/index.js
@@ -3,17 +3,31 @@ const store = require("@b-kamphorst/terminology-store");
 const path = require("path");
 
 module.exports = function (source) {
-  const importStatement = `
-import Glossary from "${this.query.glossaryComponentPath || "@b-kamphorst/docusaurus-glossary-view"}";
-  `;
+  // Extract front matter (--- ... ---)
+  const frontMatterMatch = source.match(/^---\r?\n[\s\S]*?\r?\n---(\r?\n|$)/);
+  const frontMatter = frontMatterMatch ? frontMatterMatch[0] : "";
+  const contentWithoutFrontMatter = frontMatterMatch
+    ? source.slice(frontMatterMatch[0].length)
+    : source;
+
+  // Prepare the import statement
+  const importStatement = `import Glossary from "${this.query.glossaryComponentPath || "@b-kamphorst/docusaurus-glossary-view"}";\n\n`;
+
+  // Only add the import if it’s not already in the source
+  const contentAlreadyHasImport =
+    contentWithoutFrontMatter.includes(importStatement);
+
+  source =
+    frontMatter +
+    (contentAlreadyHasImport ? "" : importStatement) +
+    contentWithoutFrontMatter;
+
   this.cacheable(false);
   this.addDependency(path.posix.join(this.query.docsDir, "glossary.json"));
   this.emitFile(
     path.posix.join(this.query.docsDir, "glossary.json"),
     JSON.stringify(store.terms),
   );
-  const { content } = parseMD(source);
-  source = source.replace(content, importStatement + content);
   source += `
 
   <Glossary />
